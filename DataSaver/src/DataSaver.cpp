dfc12484c240a5ff41bee76592a6ba6763c5959d
Zassa Kavuma <zaskav@hotmail.com>
2013-01-30 11:03:28 +0000

    Update copyright terms (DataSaver)

--- DataSaver/src/DataSaver.cpp
+++ DataSaver/src/DataSaver.cpp
@@ -2,7 +2,7 @@
 // Name        : DataSaver.cpp
 // Author      : Zassa Kavuma
 // Version     : 0.1
-// Copyright   : Open Source
+// Copyright   : Released under the GNU General Public License
 // Description : Program for saving neural network training data
 //============================================================================
 
